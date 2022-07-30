24509b81282ff0d16f47f934105bf4fc76633bcb
skynetcap <100323448+skynetcap@users.noreply.github.com>
2022-07-29 20:07:50 -0700

    Implement SerumOrderManager

--- src/main/java/com/mmorrell/App.java
+++ src/main/java/com/mmorrell/App.java
@@ -1,13 +1,30 @@
 package com.mmorrell;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        // create order manager (pkey, rpcclient)
+        SerumOrderManager orderManager = new SerumOrderManager();
+
+        // configure strategy parameters
+        orderManager.setSolAmount(0.5);
+
+        // executorservice, start runnable containing loop'
+        executor.submit(() -> {
+            while (true) {
+                orderManager.executeArb();
+                Thread.sleep(500L);
+            }
+        });
+
+        System.out.println("Serum arb bot started.");
     }
 }
--- src/main/java/com/mmorrell/SerumOrderManager.java
+++ src/main/java/com/mmorrell/SerumOrderManager.java
@@ -1,8 +1,34 @@
 package com.mmorrell;
 
+import lombok.extern.slf4j.Slf4j;
+import org.p2p.solanaj.core.Account;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Slf4j
 public class SerumOrderManager {
 
+    private final Account account;
+    private double solAmount;
 
+    public SerumOrderManager() {
+        try {
+            this.account = Account.fromJson(Files.readString(Paths.get("src/main/resources/mainnet.json")));
+            log.info("Loaded wallet: " + account.getPublicKey().toBase58());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void setSolAmount(double solAmount) {
+        this.solAmount = solAmount;
+    }
 
+    public void executeArb() {
+        // get sol/usdc asks
+
+        // get sol/usdt bids
+    }
 }
